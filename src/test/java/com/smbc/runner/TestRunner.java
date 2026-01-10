89ec972d38ac6cf6c97d86d90282e6b2e8a7810d
khurram-syed <khurram245@yahoo.com>
2026-01-10 18:11:19 +0000

    first commit

---
+++ src/test/java/com/smbc/runner/TestRunner.java
@@ -0,0 +1,22 @@
+package com.smbc.runner;
+
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.smbc.steps")
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/reports/cucumber-report.html, json:target/reports/cucumber-report.json")
+
+public class TestRunner {
+
+}
